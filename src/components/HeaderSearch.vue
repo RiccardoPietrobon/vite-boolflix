83b457e5de3194d1a999aa2dd05d9fa4ad2bae36
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-03-03 18:56:29 +0100

    boot

---
+++ src/components/HeaderSearch.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default {
+  data() {
+    return {
+      title: "Header",
+    }
+  }
+
+  // components: {
+  //   MyComponent,
+  // },
+};
+</script>
+
+<template>
+  <div class="bg-dark">
+
+    <div class="header d-flex container p-5">
+      <h1 class="text-danger">BOOLFLIX</h1>
+      <div class="input-group input-group-sm">
+        <input type="text" class="form-control" placeholder="Recipient's username"
+          aria-label="Recipient's username with two button addons" aria-describedby="inputGroup-sizing-sm">
+        <button class="btn btn-outline-secondary" type="button">Button</button>
+        <button class="btn btn-outline-secondary" type="button">Button</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/MainPrincipal.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+    data() {
+        return {
+            title: "Main",
+        }
+    }
+
+};
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss" scoped></style>
